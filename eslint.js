230e2e74bdbf747e5b17cc3480f0b785f6cc45bd
michal@vantage <cz@signicode.com>
2019-01-16 13:03:30 +0100

    Update

--- eslint.js
+++ eslint.js
@@ -1,21 +1,19 @@
 module.exports = {
     "plugins": ["node"],
-    "configs": {
-        "recommended": {
-            "extends": ["eslint:recommended", "plugin:node/recommended"],
-        },
-        "rules": {
-            "node/exports-style": ["error", "module.exports"],
-            "quotes": ["error", "double"],
-            "semi": [2, "always"],
-            "linebreak-style": ["error", "unix"],
-            "indent": ["error", 4],
-            "no-extra-semi": 2,
-            "semi-spacing": [2, { "before": false, "after": true }]
-        },
-        "env": {
-            "es6": true,
-            "node": true
-        }
+    "recommended": {
+        "extends": ["eslint:recommended", "plugin:node/recommended"],
+    },
+    "rules": {
+        "node/exports-style": ["error", "module.exports"],
+        "quotes": ["error", "double"],
+        "semi": [2, "always"],
+        "linebreak-style": ["error", "unix"],
+        "indent": ["error", 4],
+        "no-extra-semi": 2,
+        "semi-spacing": [2, { "before": false, "after": true }]
+    },
+    "env": {
+        "es6": true,
+        "node": true
     }
 };
